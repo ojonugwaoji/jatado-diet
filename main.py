89be8075628dfa8390e3c7885a82658f1da4e339
ojinugwa <45524127+ojinugwa@users.noreply.github.com>
2023-12-13 12:00:43 -0500

    Portfix for heroku

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import uvicorn
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=3000, reload=True)
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run("app.main:app", port=port, reload=True)
