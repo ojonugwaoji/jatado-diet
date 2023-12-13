a83cc679df74b9d93349336522baf6f70da1f3ba
ojinugwa <45524127+ojinugwa@users.noreply.github.com>
2023-12-13 12:17:10 -0500

    port fix 2

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("app.main:app", port=port, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
