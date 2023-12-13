381ba3f88c01a7f42036f1a74da92bd196128097
ojinugwa <45524127+ojinugwa@users.noreply.github.com>
2023-12-13 12:38:02 -0500

    port 3

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import os
 load_dotenv()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT"))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
